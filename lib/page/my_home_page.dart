7118464657c96c9c9cd5a3eac35a8681391b7e4e
raon <sunwonsw95@naver.com>
2023-03-17 18:39:53 +0900

    rfc:02-remove every linting errors - Lint로 나타난 메시지 모두 처리

--- lib/page/my_home_page.dart
+++ lib/page/my_home_page.dart
@@ -1,10 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_switch/flutter_switch.dart';
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -19,7 +20,7 @@ class _MyHomePageState extends State<MyHomePage> {
   bool isSwitchOn = false;
 
   Color _textColor = Colors.black;
-  Color _appBarColor = Color.fromRGBO(36, 41, 46, 1);
+  Color _appBarColor = const Color.fromRGBO(36, 41, 46, 1);
   Color _scaffoldBgcolor = Colors.white;
 
   @override
@@ -27,15 +28,15 @@ class _MyHomePageState extends State<MyHomePage> {
     return Theme(
       data: ThemeData(
         textTheme: TextTheme(
-          bodyText1: TextStyle(color: _textColor),
-          bodyText2: TextStyle(color: _textColor),
+          bodyLarge: TextStyle(color: _textColor),
+          bodyMedium: TextStyle(color: _textColor),
         ),
       ),
       child: Scaffold(
         backgroundColor: _scaffoldBgcolor,
         appBar: AppBar(
           backgroundColor: _appBarColor,
-          title: Text(
+          title: const Text(
             "FlutterSwitch Demo",
             style: TextStyle(color: Colors.white),
           ),
@@ -52,12 +53,12 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text("Default"),
-                SizedBox(height: 10.0),
+                const Text("Default"),
+                const SizedBox(height: 10.0),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -77,9 +78,9 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ],
                 ),
-                SizedBox(height: 20.0),
-                Text("Custom Colors and Borders"),
-                SizedBox(height: 10.0),
+                const SizedBox(height: 20.0),
+                const Text("Custom Colors and Borders"),
+                const SizedBox(height: 10.0),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -90,16 +91,16 @@ class _MyHomePageState extends State<MyHomePage> {
                       value: status2,
                       borderRadius: 30.0,
                       padding: 2.0,
-                      toggleColor: Color.fromRGBO(225, 225, 225, 1),
+                      toggleColor: const Color.fromRGBO(225, 225, 225, 1),
                       switchBorder: Border.all(
-                        color: Color.fromRGBO(2, 107, 206, 1),
+                        color: const Color.fromRGBO(2, 107, 206, 1),
                         width: 6.0,
                       ),
                       toggleBorder: Border.all(
-                        color: Color.fromRGBO(2, 107, 206, 1),
+                        color: const Color.fromRGBO(2, 107, 206, 1),
                         width: 5.0,
                       ),
-                      activeColor: Color.fromRGBO(51, 226, 255, 1),
+                      activeColor: const Color.fromRGBO(51, 226, 255, 1),
                       inactiveColor: Colors.black38,
                       onToggle: (val) {
                         setState(() {
@@ -115,9 +116,9 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ],
                 ),
-                SizedBox(height: 20.0),
-                Text("With 'On' and 'Off' text and custom text colors"),
-                SizedBox(height: 10.0),
+                const SizedBox(height: 20.0),
+                const Text("With 'On' and 'Off' text and custom text colors"),
+                const SizedBox(height: 10.0),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -140,9 +141,9 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ],
                 ),
-                SizedBox(height: 20.0),
-                Text("Custom size"),
-                SizedBox(height: 10.0),
+                const SizedBox(height: 20.0),
+                const Text("Custom size"),
+                const SizedBox(height: 10.0),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -166,9 +167,9 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ],
                 ),
-                SizedBox(height: 20.0),
-                Text("Custom border radius and padding"),
-                SizedBox(height: 10.0),
+                const SizedBox(height: 20.0),
+                const Text("Custom border radius and padding"),
+                const SizedBox(height: 10.0),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -195,9 +196,9 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ],
                 ),
-                SizedBox(height: 20.0),
-                Text("Custom text"),
-                SizedBox(height: 10.0),
+                const SizedBox(height: 20.0),
+                const Text("Custom text"),
+                const SizedBox(height: 10.0),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -223,16 +224,16 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ],
                 ),
-                SizedBox(height: 20.0),
-                Text("Icon in toggle"),
-                Text(
+                const SizedBox(height: 20.0),
+                const Text("Icon in toggle"),
+                const Text(
                   "Inspired by the colors from Github Dark Mode switch",
                   style: TextStyle(
                     fontSize: 12.0,
                     fontStyle: FontStyle.italic,
                   ),
                 ),
-                SizedBox(height: 10.0),
+                const SizedBox(height: 10.0),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -243,23 +244,23 @@ class _MyHomePageState extends State<MyHomePage> {
                       value: status7,
                       borderRadius: 30.0,
                       padding: 2.0,
-                      activeToggleColor: Color(0xFF6E40C9),
-                      inactiveToggleColor: Color(0xFF2F363D),
+                      activeToggleColor: const Color(0xFF6E40C9),
+                      inactiveToggleColor: const Color(0xFF2F363D),
                       activeSwitchBorder: Border.all(
-                        color: Color(0xFF3C1E70),
+                        color: const Color(0xFF3C1E70),
                         width: 6.0,
                       ),
                       inactiveSwitchBorder: Border.all(
-                        color: Color(0xFFD1D5DA),
+                        color: const Color(0xFFD1D5DA),
                         width: 6.0,
                       ),
-                      activeColor: Color(0xFF271052),
+                      activeColor: const Color(0xFF271052),
                       inactiveColor: Colors.white,
-                      activeIcon: Icon(
+                      activeIcon: const Icon(
                         Icons.nightlight_round,
                         color: Color(0xFFF8E3A1),
                       ),
-                      inactiveIcon: Icon(
+                      inactiveIcon: const Icon(
                         Icons.wb_sunny,
                         color: Color(0xFFFFDF5D),
                       ),
@@ -269,11 +270,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
                           if (val) {
                             _textColor = Colors.white;
-                            _appBarColor = Color.fromRGBO(22, 27, 34, 1);
-                            _scaffoldBgcolor = Color(0xFF0D1117);
+                            _appBarColor = const Color.fromRGBO(22, 27, 34, 1);
+                            _scaffoldBgcolor = const Color(0xFF0D1117);
                           } else {
                             _textColor = Colors.black;
-                            _appBarColor = Color.fromRGBO(36, 41, 46, 1);
+                            _appBarColor = const Color.fromRGBO(36, 41, 46, 1);
                             _scaffoldBgcolor = Colors.white;
                           }
                         });
@@ -285,9 +286,9 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ],
                 ),
-                SizedBox(height: 20.0),
-                Text("Image as toggle icon"),
-                SizedBox(height: 10.0),
+                const SizedBox(height: 20.0),
+                const Text("Image as toggle icon"),
+                const SizedBox(height: 10.0),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -298,18 +299,18 @@ class _MyHomePageState extends State<MyHomePage> {
                       value: status8,
                       borderRadius: 30.0,
                       padding: 2.0,
-                      activeToggleColor: Color(0xFF0082C8),
-                      inactiveToggleColor: Color(0xFF01579B),
+                      activeToggleColor: const Color(0xFF0082C8),
+                      inactiveToggleColor: const Color(0xFF01579B),
                       activeSwitchBorder: Border.all(
-                        color: Color(0xFF00D2B8),
+                        color: const Color(0xFF00D2B8),
                         width: 6.0,
                       ),
                       inactiveSwitchBorder: Border.all(
-                        color: Color(0xFF29B6F6),
+                        color: const Color(0xFF29B6F6),
                         width: 6.0,
                       ),
-                      activeColor: Color(0xFF55DDCA),
-                      inactiveColor: Color(0xFF54C5F8),
+                      activeColor: const Color(0xFF55DDCA),
+                      inactiveColor: const Color(0xFF54C5F8),
                       activeIcon: Image.network(
                         "https://img2.pngio.com/functional-bits-in-flutter-flutter-community-medium-flutter-png-1000_1000.png",
                       ),
