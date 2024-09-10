329c060c1fb058b8d813ede6a18bbd0db96db582
bowon lee <asdfg3678@gmail.com>
2024-09-10 23:23:31 +0900

    테마파일 구성요소 변경

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 final ThemeData WHITE_THEME = ThemeData(
+  scaffoldBackgroundColor: Colors.white,
+  appBarTheme: const AppBarTheme(color: Color.fromRGBO(36, 41, 46, 1)),
   textTheme: const TextTheme(
     bodySmall: TextStyle(color: Colors.black),
-    bodyMedium: TextStyle(color: Color.fromRGBO(36, 41, 46, 1)),
-    bodyLarge: TextStyle(color: Colors.white),
+    bodyMedium: TextStyle(color: Colors.black),
+    bodyLarge: TextStyle(color: Colors.black),
   ),
 );
 
 final ThemeData DARK_THEME = ThemeData(
+    scaffoldBackgroundColor: const Color(0xFF0D1117),
+    appBarTheme: const AppBarTheme(color: Color.fromRGBO(22, 27, 34, 1)),
     textTheme: const TextTheme(
         bodySmall: TextStyle(color: Colors.white),
-        bodyMedium: TextStyle(color: Color.fromRGBO(22, 27, 34, 1)),
-        bodyLarge: TextStyle(color: Color(0xFF0D1117))));
+        bodyMedium: TextStyle(color: Colors.white),
+        bodyLarge: TextStyle(color: Colors.white)));
