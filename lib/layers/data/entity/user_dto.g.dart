151d1b2269ebb6d247fccb8e3e99f363db9727c4
parksang-won <tkddnjs2575@naver.com>
2024-11-14 15:07:35 +0900

    Etc: runner_builder file add

---
+++ lib/layers/data/entity/user_dto.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserDTO _$UserDTOFromJson(Map<String, dynamic> json) => UserDTO(
+      userId: json['userId'] as String,
+      name: json['name'] as String,
+      email: json['email'] as String,
+      profilePicture: json['profilePicture'] as String,
+      status: json['status'] as String,
+      role: json['role'] as String,
+    );
+
+Map<String, dynamic> _$UserDTOToJson(UserDTO instance) => <String, dynamic>{
+      'userId': instance.userId,
+      'name': instance.name,
+      'email': instance.email,
+      'profilePicture': instance.profilePicture,
+      'status': instance.status,
+      'role': instance.role,
+    };
