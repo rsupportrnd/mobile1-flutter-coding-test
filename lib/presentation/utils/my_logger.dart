e51ade5f342e23f4210e2e44f9d8720468d3d20c
jeongwoobin <woobin406@naver.com>
2025-07-01 15:28:32 +0900

    [feat] logger 추가

---
+++ lib/presentation/utils/my_logger.dart
@@ -0,0 +1,55 @@
+import 'package:logger/logger.dart';
+
+class Log {
+  late Logger _logger;
+
+  static Logger to = _instance._logger;
+  factory Log() => _instance;
+  static final Log _instance = Log._internal();
+  Log._internal() {
+    // initCode
+    _logger = Logger(
+      output: ConsoleOutput(),
+      printer: PrettyPrinter(
+        methodCount: 2, // number of method calls to be displayed
+        errorMethodCount: 8, // number of method calls if stacktrace is provided
+        lineLength: 100, // width of the output
+        colors: true, // Colorful log messages
+        printEmojis: true, // Print an emoji for each log message
+        // dateTimeFormat: true // Should each log print contain a timestamp
+      ),
+      // level: Level.trace
+    ); //kDebugMode ? Level.verbose : Level.nothing);
+  }
+
+  static void t(dynamic message) {
+    to.t(message);
+  }
+
+  static void d(dynamic message) {
+    to.d(message);
+  }
+
+  static void i(dynamic message) {
+    to.i(message);
+  }
+
+  static void w(dynamic message) {
+    to.w(message);
+  }
+
+  static void e(dynamic message) {
+    to.e(message);
+  }
+
+  static void f(dynamic message) {
+    to.f(message);
+  }
+}
+
+class Filter extends LogFilter {
+  @override
+  bool shouldLog(LogEvent event) {
+    return true;
+  }
+}
