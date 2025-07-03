45d7ab9d9e5eea476cec0dbca708a9f5215375e0
jin <ihatebeans3000@gmail.com>
2025-07-04 02:58:51 +0900

    feat : usecase내 이벤트 스트림 추가

--- lib/domain/usecases/get_room_message_usecase.dart
+++ lib/domain/usecases/get_room_message_usecase.dart
@@ -1,13 +1,32 @@
+import 'dart:async';
+
 import 'package:mobile1_flutter_coding_test/domain/entities/message_entity.dart';
 import 'package:mobile1_flutter_coding_test/domain/repositories/message_repository.dart';
+import 'package:rxdart/rxdart.dart';
 
 class GetRoomMessageUseCase {
   final MessageRepository repository;
+  final _updateStream = PublishSubject<List<MessageEntity>>();
 
   GetRoomMessageUseCase(this.repository);
 
+  StreamSubscription<List<MessageEntity>> get stream {
+    StreamSubscription<List<MessageEntity>> sub = _updateStream.listen((event) {});
+    return sub;
+  }
+
   Future<List<MessageEntity>> call({required String roomId}) async {
     final messages = await repository.getMessages();
-    return messages.where((item) => item.roomId == roomId).toList();
+    final filtered = messages.where((m) => m.roomId == roomId).toList();
+    _updateStream.add(filtered);
+    return filtered;
+  }
+
+  void removeListener(StreamSubscription<List<MessageEntity>> subscription) {
+    subscription.cancel();
+  }
+
+  void dispose() {
+    _updateStream.close();
   }
 }
--- lib/domain/usecases/get_rooms_usecase.dart
+++ lib/domain/usecases/get_rooms_usecase.dart
@@ -1,12 +1,27 @@
+import 'dart:async';
+
 import 'package:mobile1_flutter_coding_test/domain/entities/room_entity.dart';
 import 'package:mobile1_flutter_coding_test/domain/repositories/room_repository.dart';
+import 'package:rxdart/rxdart.dart';
 
 class GetRoomsUseCase {
   final RoomRepository repository;
+  final _updateStream = PublishSubject<List<RoomEntity>>();
 
   GetRoomsUseCase(this.repository);
 
+  StreamSubscription<List<RoomEntity>> get stream {
+    StreamSubscription<List<RoomEntity>> sub = _updateStream.listen((event) {});
+    return sub;
+  }
+
   Future<List<RoomEntity>> call() async {
-    return await repository.getRooms();
+    final rooms = await repository.getRooms();
+    _updateStream.add(rooms);
+    return rooms;
+  }
+
+  void dispose() {
+    _updateStream.close();
   }
 }
