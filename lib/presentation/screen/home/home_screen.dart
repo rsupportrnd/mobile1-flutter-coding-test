24a852e5bd3f90828e26369f8270338e153fb4ab
jeongwoobin <woobin406@naver.com>
2025-07-04 15:23:04 +0900

    [feat] 홈화면에서 뒤로가기 시 토스트로 한번 더 눌러야 종료됨을 알림

--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:mobile1_flutter_coding_test/di/viewmodel_provider.dart';
 import 'package:mobile1_flutter_coding_test/presentation/screen/meeting_list/meeting_list_screen.dart';
 import 'package:mobile1_flutter_coding_test/presentation/screen/user_list/user_list_screen.dart';
@@ -14,6 +16,7 @@ class HomeScreen extends ConsumerStatefulWidget {
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
   late Future<void> _loadFuture;
+  DateTime? _lastBackPressTime;
 
   @override
   void initState() {
@@ -29,43 +32,62 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
       MainTab.users: const UserListScreen(),
       MainTab.meetings: const MeetingListScreen(),
     };
-    return Scaffold(
-      body: FutureBuilder(
-          future: _loadFuture,
-          builder: (context, asyncSnapshot) {
-            if (asyncSnapshot.connectionState == ConnectionState.done) {
-              if (asyncSnapshot.hasError) {
-                return const Center(child: Text("오류가 발생하였습니다."));
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) async {
+        final now = DateTime.now();
+        if (didPop == false && _lastBackPressTime == null ||
+            now.difference(_lastBackPressTime!) > const Duration(seconds: 2)) {
+          _lastBackPressTime = now;
+
+          Fluttertoast.showToast(
+            msg: "한 번 더 누르면 앱이 종료됩니다.",
+            gravity: ToastGravity.BOTTOM,
+            toastLength: Toast.LENGTH_SHORT,
+          );
+        } else {
+          // 2초 내에 한번 더 눌렀을 시
+          SystemNavigator.pop();
+        }
+      },
+      child: Scaffold(
+        body: FutureBuilder(
+            future: _loadFuture,
+            builder: (context, asyncSnapshot) {
+              if (asyncSnapshot.connectionState == ConnectionState.done) {
+                if (asyncSnapshot.hasError) {
+                  return const Center(child: Text("오류가 발생하였습니다."));
+                }
+                return Center(
+                    child: screens[selectedTab] ??
+                        const Text("화면을 불러오는 중 오류가 발생하였습니다."));
+              } else {
+                return const Center(
+                    child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("데이터 불러오는중..."),
+                    SizedBox(height: 10),
+                    IgnorePointer(
+                      child: CircularProgressIndicator(),
+                    ),
+                  ],
+                ));
               }
-              return Center(
-                  child: screens[selectedTab] ??
-                      const Text("화면을 불러오는 중 오류가 발생하였습니다."));
-            } else {
-              return const Center(
-                  child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text("데이터 불러오는중..."),
-                  SizedBox(height: 10),
-                  IgnorePointer(
-                    child: CircularProgressIndicator(),
-                  ),
-                ],
-              ));
-            }
-          }),
-      bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Users'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.meeting_room), label: 'Meetings'),
-          ],
-          currentIndex: MainTab.values.indexOf(selectedTab),
-          onTap: (index) {
-            ref
-                .read(homeViewModelProvider.notifier)
-                .changeTab(tab: MainTab.values[index]);
-          }),
+            }),
+        bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Users'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.meeting_room), label: 'Meetings'),
+            ],
+            currentIndex: MainTab.values.indexOf(selectedTab),
+            onTap: (index) {
+              ref
+                  .read(homeViewModelProvider.notifier)
+                  .changeTab(tab: MainTab.values[index]);
+            }),
+      ),
     );
   }
 }
