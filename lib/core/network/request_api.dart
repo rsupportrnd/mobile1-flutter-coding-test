ff40beb4f195fcae6f4ab24b61678620a979593b
ws5260 <ws5260@naver.com>
2025-09-02 09:48:31 +0900

    Feat: Network Get 요청 기능 추가

--- lib/core/network/request_api.dart
+++ lib/core/network/request_api.dart
@@ -1,10 +1,13 @@
-import 'package:mobile1_flutter_coding_test/data/model/response_result.dart';
+import 'package:mobile1_flutter_coding_test/core/network/response_result.dart';
 import 'package:dio/dio.dart';
 
 class RequestApi {
-  static Future<ResponseResult<T>> request<T>(String url,
+  static const String baseUrl =
+      "https://raw.githubusercontent.com/rsupportrnd/mobile1-flutter-coding-test/refs/heads/main/api/";
+  static Future<ResponseResult<T>> request<T>(String endPoint,
       {required T Function(Map<String, dynamic>) fromJson}) async {
-    final response = await Dio().get(url);
+    final dio = Dio();
+    final response = await dio.get("$baseUrl$endPoint");
     return ResponseResult(
       code: response.statusCode ?? 0,
       data: fromJson(response.data),
