3cf2f926fa896a379fe9ee43df2822e961b54699
smchoi <smchoi@maprex.co.kr>
2025-09-04 02:44:20 +0900

    Feat: 사용자 목록에 사용자 정보를 표기할 User Card 위젯 추가

---
+++ lib/presentation/user/component/user_card.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobile1_flutter_coding_test/domain/entity/user.dart';
+import 'package:mobile1_flutter_coding_test/shared/enum/role_type.dart';
+import 'package:mobile1_flutter_coding_test/shared/enum/user_status.dart';
+import 'package:mobile1_flutter_coding_test/shared/widgets/profile.dart';
+import 'package:mobile1_flutter_coding_test/shared/widgets/tag.dart';
+
+class UserCard extends ConsumerWidget {
+  const UserCard({super.key, required this.user});
+  final User user;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFFF6F7F8),
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(
+          color: Colors.grey.withValues(alpha: 0.1),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          children: [
+            // Profile Image
+            CircleAvatar(
+              child: Profile(
+                imageUrl: user.profilePicture,
+              ),
+            ),
+            const SizedBox(width: 16),
+
+            // User Info
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    user.name,
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    "ws5260@naver.com",
+                  ),
+                ],
+              ),
+            ),
+
+            // Status and Role Tags
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Tag.fromUserStatus(UserStatus.online),
+                const SizedBox(height: 2),
+                Tag.fromRoleType(RoleType.admin),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
