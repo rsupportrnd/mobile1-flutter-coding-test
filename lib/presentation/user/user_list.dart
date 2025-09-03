adda91a434707f26a07bda1c8e961d2c31a42782
smchoi <smchoi@maprex.co.kr>
2025-09-04 02:45:40 +0900

    Feat: 사용자 목록 페이지 정의
    - 사용자 목록 페이지 정의
    - 라우터에 사용자 목록 페이지 적용

---
+++ lib/presentation/user/user_list.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
+import 'package:mobile1_flutter_coding_test/domain/entity/user.dart';
+import 'package:mobile1_flutter_coding_test/presentation/user/component/user_card.dart';
+import 'package:mobile1_flutter_coding_test/shared/enum/role_type.dart';
+import 'package:mobile1_flutter_coding_test/shared/enum/user_status.dart';
+import 'package:mobile1_flutter_coding_test/shared/widgets/profile.dart';
+import 'package:mobile1_flutter_coding_test/shared/widgets/tag.dart';
+
+class UserList extends ConsumerStatefulWidget {
+  const UserList({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _UserListState();
+}
+
+class _UserListState extends ConsumerState<ConsumerStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    const horizontalPadding = 16.0;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            "사용자",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Expanded(
+          child: ListView.separated(
+            itemCount: 20, // 예시 데이터 개수
+            itemBuilder: (context, index) {
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: horizontalPadding),
+                child: UserCard(
+                    user: User(
+                        userId: 'user$index',
+                        name: 'name$index',
+                        email: 'email$index',
+                        profilePicture:
+                            'https://picsum.photos/id/1011/200/200 ',
+                        status: UserStatus.online,
+                        role: RoleType.admin)),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const Gap(8);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
