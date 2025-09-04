ff1709b37c82b2b037dd6cc68c3ee702bb27ea88
smchoi <smchoi@maprex.co.kr>
2025-09-04 21:43:05 +0900

    Refactor: Hive 패키지 초기화 추가

--- lib/core/storage/hive_storage.dart
+++ lib/core/storage/hive_storage.dart
@@ -3,6 +3,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 class HiveStorage {
   String boxName = "app_box";
   static final HiveStorage _instance = HiveStorage._();
+  Box? _box;
 
   static HiveStorage get instance => _instance;
 
@@ -10,20 +11,29 @@ class HiveStorage {
 
   Future<void> init() async {
     await Hive.initFlutter();
+    _box = await Hive.openBox(boxName);
   }
 
   Future<void> put<T>(String key, T value, {String? boxName}) async {
-    final box = await Hive.openBox<T>(boxName ?? this.boxName);
+    final box = _box ?? await Hive.openBox(boxName ?? this.boxName);
     await box.put(key, value);
   }
 
   Future<T?> get<T>(String key, {String? boxName}) async {
-    final box = await Hive.openBox<T>(boxName ?? this.boxName);
-    return box.get(key);
+    final box = _box ?? await Hive.openBox(boxName ?? this.boxName);
+    final value = box.get(key);
+
+    if (value == null) return null;
+
+    try {
+      return value as T;
+    } catch (e) {
+      return null;
+    }
   }
 
   Future<void> delete<T>(String key, {String? boxName}) async {
-    final box = await Hive.openBox<T>(boxName ?? this.boxName);
+    final box = _box ?? await Hive.openBox(boxName ?? this.boxName);
     box.delete(key);
   }
 
