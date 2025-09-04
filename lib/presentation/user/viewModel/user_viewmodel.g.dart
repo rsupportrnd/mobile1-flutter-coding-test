f7f78041d4bb1df5b799b2c3df3f45070e9d344a
smchoi <smchoi@maprex.co.kr>
2025-09-04 10:33:32 +0900

    Feat: user viewmodel 추가

---
+++ lib/presentation/user/viewModel/user_viewmodel.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_viewmodel.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userViewModelHash() => r'409a84061857b23136c1349086378569233d28eb';
+
+/// See also [UserViewModel].
+@ProviderFor(UserViewModel)
+final userViewModelProvider =
+    AutoDisposeAsyncNotifierProvider<UserViewModel, UserState>.internal(
+  UserViewModel.new,
+  name: r'userViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$userViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UserViewModel = AutoDisposeAsyncNotifier<UserState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
