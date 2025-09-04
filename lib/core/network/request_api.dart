9b120e1c88c725f94a132f637e20f45f5095177d
smchoi <smchoi@maprex.co.kr>
2025-09-04 11:02:47 +0900

    Feat: 에러코드 타입 및 에러코드 정의

--- lib/core/network/request_api.dart
+++ lib/core/network/request_api.dart
@@ -1,13 +1,25 @@
+import 'package:mobile1_flutter_coding_test/core/network/response_codes.dart';
 import 'package:mobile1_flutter_coding_test/data/model/response_result.dart';
 import 'package:dio/dio.dart';
 
 class RequestApi {
   static Future<ResponseResult<T>> request<T>(String url,
-      {required T Function(Map<String, dynamic>) fromJson}) async {
-    final response = await Dio().get(url);
-    return ResponseResult(
-      code: response.statusCode ?? 0,
-      data: fromJson(response.data),
-    );
+      {required T? Function(Map<String, dynamic>) fromJson}) async {
+    try {
+      final response = await Dio().get(url);
+
+      if (response.statusCode == 200) {
+        return ResponseResult(
+          code: ResponseCodes.success,
+          data: fromJson(response.data),
+        );
+      }
+      return ResponseResult(
+          code: ResponseCodes.fromCode(response.statusCode.toString()));
+    } catch (e) {
+      return ResponseResult(
+        code: ResponseCodes.unkownError,
+      );
+    }
   }
 }
