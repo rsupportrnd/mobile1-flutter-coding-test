f56d6c272e521efbf3b890ea23f9fb60dc958745
smchoi <smchoi@maprex.co.kr>
2025-09-04 11:59:06 +0900

    Fix: Merge하면서 누락된 BaseUrl 다시 추가

--- lib/core/network/request_api.dart
+++ lib/core/network/request_api.dart
@@ -5,10 +5,12 @@ import 'package:dio/dio.dart';
 import 'package:mobile1_flutter_coding_test/core/network/response_result.dart';
 
 class RequestApi {
-  static Future<ResponseResult<T>> request<T>(String url,
+  static const baseUrl =
+      "https://raw.githubusercontent.com/rsupportrnd/mobile1-flutter-coding-test/refs/heads/main/api/";
+  static Future<ResponseResult<T>> request<T>(String endpoint,
       {required T? Function(Map<String, dynamic>) fromJson}) async {
     try {
-      final response = await Dio().get(url);
+      final response = await Dio().get("$baseUrl$endpoint");
 
       if (response.statusCode == 200) {
         Map<String, dynamic> json = jsonDecode(response.data);
