2d049c4d82754428db90c7b0178987d2b5c214ac
bowon lee <asdfg3678@gmail.com>
2025-09-29 14:02:20 +0900

    홈 네비게이터 구현, 홈 화면을 기본으로 구성

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,16 +1,16 @@
 import 'package:go_router/go_router.dart';
-import 'package:mobile1_flutter_coding_test/features/user/presentation/pages/user_list_page.dart';
+import 'package:mobile1_flutter_coding_test/features/home/presentation/pages/home_page.dart';
 
 class AppRouter {
-  static const String userList = '/';
+  static const String home = '/';
 
   static final GoRouter router = GoRouter(
-    initialLocation: userList,
+    initialLocation: home,
     routes: [
       GoRoute(
-        path: userList,
-        name: 'userList',
-        builder: (context, state) => const UserListPage(),
+        path: home,
+        name: 'home',
+        builder: (context, state) => const HomePage(),
       ),
     ],
   );
